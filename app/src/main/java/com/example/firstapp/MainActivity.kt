7ac89bec4f7c8e9ee9c82c8f409ddf91901d1e9e
unknown <Administrator@KANINI-LTP-460.gisbizdev.gisbiz.chennai.com>
2021-12-03 17:53:37 +0530

    Text View

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -6,24 +6,24 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        var color="";
+        var color=""
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val Red_button=findViewById<TextView>(R.id.red);
-        val Green_button=findViewById<TextView>(R.id.green);
-        val Blue_button=findViewById<TextView>(R.id.blue);
-        val text=findViewById<TextView>(R.id.box);
-        Red_button.setOnClickListener({
-            color="Red";
-            text.text="$color was clicked";
-        });
-        Green_button.setOnClickListener({
-            color="Green";
-            text.text="$color was clicked";
-        });
-        Blue_button.setOnClickListener({
-            color="Blue";
-            text.text="$color was clicked";
-        })
+        val Red_button=findViewById<TextView>(R.id.red)
+        val Green_button=findViewById<TextView>(R.id.green)
+        val Blue_button=findViewById<TextView>(R.id.blue)
+        val text=findViewById<TextView>(R.id.box)
+        Red_button.setOnClickListener{
+            color="Red"
+            text.setText("$color was clicked")
+        }
+        Green_button.setOnClickListener{
+            color="Green"
+            text.setText("$color was clicked")
+        }
+        Blue_button.setOnClickListener{
+            color="Blue"
+            text.setText("$color was clicked")
+        }
     }
 }
